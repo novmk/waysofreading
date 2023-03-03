074fcf402d5934d83333b55c26aec28a9c504ce1
novmk <67250036+novmk@users.noreply.github.com>
2023-03-03 12:06:42 +0000

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -7,6 +7,15 @@ $(document).ready(function() {
     //     e.preventDefault();
     //     $(this).toggleClass('list_ani');
     // });
+
+    $(".scroll").click(function (){
+        var toLink = $(this).attr('alt');
+        var linkTop = $('#' + toLink).offset().top - $('#' + toLink).height()/2.71;
+        console.log(linkTop);
+        $('html, body').animate({
+            scrollTop: linkTop
+        }, 2000);
+    });
 });
 
 
